93dde045d81f3c98571c1787fc5d4f1dd65adcd7
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-12-05 17:54:33 +0530

    If-else

---
+++ If-else/One.java
@@ -0,0 +1,10 @@
+class One{
+    public static void main(String[] args) {
+        boolean rain = true;
+        if(rain){
+            System.out.println("Take an umberlla");
+        }else{
+            System.out.println("Enjoy the sunlight");
+        }
+    }
+}
