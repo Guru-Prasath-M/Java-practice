f2f7c09e61d8e91f172773f82528e6b4e48e1483
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-12-08 15:37:49 +0530

    String Comparison

--- String Comparison/Task.java
+++ String Comparison/Task.java
@@ -1,8 +1,9 @@
 import java.util.*;
 class Task{
     public static void main(String[] args) {
-        String RCB = "win";
-        if(RCB.equals(RCB)){
+        Scanner sc = new Scanner(System.in);
+        String RCB = sc.nextLine();
+        if(RCB.equals("win")){
             System.out.println("Ea sala cup namdheyy");
         }else{
             System.out.println("Better luck next time");
---
+++ String Comparison/TaskTwo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class TaskTwo{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String meghana = sc.nextLine();
+        if(meghana.equals("dead")){
+            System.out.println("Surya meets Ramya");
+        }
+        else{
+            System.out.println("Surya weds Meghana");
+        }
+    }
+}
