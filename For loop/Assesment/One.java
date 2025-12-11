52a42c239987e048c867400b5173291be555c4b7
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-12-11 18:16:50 +0530

    For Loop

---
+++ For loop/Assesment/One.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class One{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number 1 : ");
+        int a = sc.nextInt();
+        System.out.println("Number 2 : ");
+        int b = sc.nextInt();
+
+        for(int i=a ; i<=b ; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ For loop/Assesment/Two.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Two{
+    public static void main(String[] args) {
+        for(int i=1 ; i<=10 ; i++){
+            if(i%2 == 0){
+                System.out.println(i);
+            }
+        }
+        
+    }
+}
